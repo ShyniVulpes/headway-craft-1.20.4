a9beb63a87e49c805a45be05136f902bc08c2491
ShyniVulpes <shynivulpes@gmail.com>
2024-02-12 21:26:00 +0300

    first commit

---
+++ src/main/java/net/shynivulpes/headwaycraft/HeadwayCraftClient.java
@@ -0,0 +1,10 @@
+package net.shynivulpes.headwaycraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HeadwayCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
